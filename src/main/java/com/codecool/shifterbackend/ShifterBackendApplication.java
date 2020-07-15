5641ff62253805439197a4ef6bc84419766daef8
Armin Kovacs <armin0191@gmail.com>
2020-07-15 09:37:01 +0200

    Initialize project

---
+++ src/main/java/com/codecool/shifterbackend/ShifterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.shifterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShifterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShifterBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/shifterbackend/ShifterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.shifterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShifterBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
