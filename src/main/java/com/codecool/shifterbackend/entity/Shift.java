3da83b3a27a80882c55b7b4dad29e4f06a784386
Armin Kovacs <armin0191@gmail.com>
2020-07-17 10:56:32 +0200

    Change inheritance strategy for shifts

--- src/main/java/com/codecool/shifterbackend/entity/Shift.java
+++ src/main/java/com/codecool/shifterbackend/entity/Shift.java
@@ -2,20 +2,18 @@ package com.codecool.shifterbackend.entity;
 
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @SuperBuilder
 @Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Shift {
     @Id
     @GeneratedValue
